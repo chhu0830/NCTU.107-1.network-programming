19b9a80f75febed19edae680b6e9615e568d670c
oHSUANo <chhu0830@gmail.com>
2018-10-13 21:49:56 +0800

    Add basic pipe

--- npshell.c
+++ npshell.c
@@ -24,12 +24,33 @@ void set_env(int argc, const char *argv[])
   setenv(argv[1], argv[2], 1);
 }
 
+int call(const char *argv[], const char *envp[], int input)
+{
+  int pfd[2], pid = 0, status;
+
+  if (pipe(pfd) < 0) return -1;
+  if ((pid = fork()) < 0) return -1;
+  else if (pid == 0) {
+    close(pfd[0]);
+    dup2(input, 0);
+    dup2(pfd[1], 1);
+    execvpe(argv[0], argv, envp);
+    exit(0);
+  }
+  else {
+    close(pfd[1]);
+    wait(&status);
+    close(input);
+    dup2(pfd[0], input);
+  }
+  return status;
+}
+
 
 int main(int argc, const char *argv[], const char *envp[])
 {
   setbuf(stdout, NULL);
   setenv("PATH", "bin:.", 1);
-
 
   char buf[MAX_INPUT_LENGTH], *command;
   char **_argv;
@@ -40,6 +61,7 @@ int main(int argc, const char *argv[], const char *envp[])
     buf[strcspn(buf, "\n")] = 0;
     if (buf[0] == 0) continue;
     char *p = buf;
+    int input = dup(0), len, flag = -1;
 
     while (command = strsep(&p, "|")) {
       wordexp(command, &exp, 0);
@@ -49,7 +71,14 @@ int main(int argc, const char *argv[], const char *envp[])
       if (strcmp(_argv[0], "printenv") == 0) print_env(_argc, _argv);
       else if (strcmp(_argv[0], "setenv") == 0) set_env(_argc, _argv);
       else if (strcmp(_argv[0], "exit") == 0) return 0;
+      else flag = call(_argv, envp, input);
     }
+
+    while(flag == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
+      buf[len] = 0;
+      fputs(buf, stdout);
+    }
+    close(input);
   }
 
   return 0;
