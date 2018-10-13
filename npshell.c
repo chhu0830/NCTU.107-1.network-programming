3a316dff63338fa5e2a04a215f8c3f5394beaf51
oHSUANo <chhu0830@gmail.com>
2018-10-13 12:15:35 +0800

    Add pipe parsing

--- npshell.c
+++ npshell.c
@@ -31,7 +31,7 @@ int main(int argc, const char *argv[], const char *envp[])
   setenv("PATH", "bin:.", 1);
 
 
-  char buf[MAX_INPUT_LENGTH];
+  char buf[MAX_INPUT_LENGTH], *command;
   char **_argv;
   int _argc;
   wordexp_t exp;
@@ -39,14 +39,17 @@ int main(int argc, const char *argv[], const char *envp[])
   while (fputs("% ", stdout), fgets(buf, MAX_INPUT_LENGTH, stdin)) {
     buf[strcspn(buf, "\n")] = 0;
     if (buf[0] == 0) continue;
+    char *p = buf;
 
-    wordexp(buf, &exp, 0);
-    _argc = exp.we_wordc;
-    _argv = exp.we_wordv;
+    while (command = strsep(&p, "|")) {
+      wordexp(command, &exp, 0);
+      _argc = exp.we_wordc;
+      _argv = exp.we_wordv;
 
-    if (strcmp(_argv[0], "printenv") == 0) print_env(_argc, _argv);
-    else if (strcmp(_argv[0], "setenv") == 0) set_env(_argc, _argv);
-    else if (strcmp(_argv[0], "exit") == 0) return 0;
+      if (strcmp(_argv[0], "printenv") == 0) print_env(_argc, _argv);
+      else if (strcmp(_argv[0], "setenv") == 0) set_env(_argc, _argv);
+      else if (strcmp(_argv[0], "exit") == 0) return 0;
+    }
   }
 
   return 0;
