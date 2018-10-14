1e08f17aaae41146447c78d3aafba8c14297b614
Chia-Hsuan Hu <chhu0830@gmail.com>
2018-10-14 23:41:35 +0800

    Add num pipe

--- npshell.c
+++ npshell.c
@@ -6,6 +6,7 @@
 
 #define MAX_COMMAND_LENGTH 256
 #define MAX_INPUT_LENGTH 256
+#define MAX_PIPE_NUM 256
 
 void print_env(int argc, const char *argv[])
 {
@@ -47,6 +48,43 @@ int call(const char *argv[], const char *envp[], int input)
   return status;
 }
 
+struct NUM_PIPE {
+  int fd, count;
+};
+
+void push(int input, struct NUM_PIPE *np, int top, int num)
+{
+  int i;
+  for (i = top - 1; i >= 0; i--) {
+    if (num >= np[i].count) np[i + 1] = np[i];
+    else break;
+  }
+  np[i + 1].count = num;
+  np[i + 1].fd = dup(input);
+}
+
+int pop(int input, struct NUM_PIPE *np, int top)
+{
+  int i, pfd[2], flag = 1, len;
+  char buf[MAX_INPUT_LENGTH];
+  for (i = 0; i < top; np[i++].count--);
+  while (top && np[top-1].count == 0) {
+    if (flag) {
+      if (pipe(pfd) < 0) return -1;
+      close(input);
+      dup2(pfd[0], input);
+      flag = 0;
+    }
+    while (len = read(np[top-1].fd, buf, MAX_INPUT_LENGTH)) {
+      write(pfd[1], buf, len);
+    }
+    top--;
+  }
+  if (flag == 0) close(pfd[1]);
+  return top;
+}
+
+
 
 int main(int argc, const char *argv[], const char *envp[])
 {
@@ -57,16 +95,23 @@ int main(int argc, const char *argv[], const char *envp[])
   char **_argv;
   int _argc;
   wordexp_t exp;
+  struct NUM_PIPE np[MAX_PIPE_NUM];
+  int num, top = 0;
 
   while (fputs("% ", stdout), fgets(buf, MAX_INPUT_LENGTH, stdin)) {
     buf[strcspn(buf, "\n")] = 0;
     if (buf[0] == 0) continue;
 
     char *p = buf, *command, *filename = NULL, *ptr;
-    int input = dup(0), output = dup(1), len, status = -1;
+    int input = dup(0), output = dup(1), len, status = -1, num = 0;
 
+    top = pop(input, np, top);
     while (command = strsep(&p, "|")) {
       if (ptr = strchr(command, '>')) *ptr = 0, filename = ptr + 2;
+      if (isdigit(command[0]) && sscanf(command, "%d", &num)) {
+        push(input, np, top++, num);
+        break;
+      }
 
       wordexp(command, &exp, 0);
       _argc = exp.we_wordc;
@@ -78,8 +123,8 @@ int main(int argc, const char *argv[], const char *envp[])
       else status = call(_argv, envp, input);
     }
 
-    if (filename) output = open(filename, O_WRONLY|O_CREAT, 0644);
-    while(status == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
+    if (filename) close(output), output = open(filename, O_WRONLY|O_CREAT, 0644);
+    while(status == 0 && num == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
       write(output, buf, len);
     }
 
