8ec35ee74ecfbac4c23adc6a64fe690d169f492c
oHSUANo <chhu0830@gmail.com>
2018-10-14 14:54:27 +0800

    Add file outputing

--- npshell.c
+++ npshell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <wordexp.h>
+#include <fcntl.h>
 
 #define MAX_COMMAND_LENGTH 256
 #define MAX_INPUT_LENGTH 256
@@ -52,7 +53,7 @@ int main(int argc, const char *argv[], const char *envp[])
   setbuf(stdout, NULL);
   setenv("PATH", "bin:.", 1);
 
-  char buf[MAX_INPUT_LENGTH], *command;
+  char buf[MAX_INPUT_LENGTH];
   char **_argv;
   int _argc;
   wordexp_t exp;
@@ -60,10 +61,13 @@ int main(int argc, const char *argv[], const char *envp[])
   while (fputs("% ", stdout), fgets(buf, MAX_INPUT_LENGTH, stdin)) {
     buf[strcspn(buf, "\n")] = 0;
     if (buf[0] == 0) continue;
-    char *p = buf;
-    int input = dup(0), len, flag = -1;
+
+    char *p = buf, *command, *filename = NULL, *ptr;
+    int input = dup(0), output = dup(1), len, status = -1;
 
     while (command = strsep(&p, "|")) {
+      if (ptr = strchr(command, '>')) *ptr = 0, filename = ptr + 2;
+
       wordexp(command, &exp, 0);
       _argc = exp.we_wordc;
       _argv = exp.we_wordv;
@@ -71,14 +75,16 @@ int main(int argc, const char *argv[], const char *envp[])
       if (strcmp(_argv[0], "printenv") == 0) print_env(_argc, _argv);
       else if (strcmp(_argv[0], "setenv") == 0) set_env(_argc, _argv);
       else if (strcmp(_argv[0], "exit") == 0) return 0;
-      else flag = call(_argv, envp, input);
+      else status = call(_argv, envp, input);
     }
 
-    while(flag == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
-      buf[len] = 0;
-      fputs(buf, stdout);
+    if (filename) output = open(filename, O_WRONLY|O_CREAT, 0644);
+    while(status == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
+      write(output, buf, len);
     }
+
     close(input);
+    close(output);
   }
 
   return 0;
