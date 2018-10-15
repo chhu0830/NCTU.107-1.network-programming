53c5435a5cf43029f03c62d2275c53c5e2ecba94
Chia-Hsuan Hu <chhu0830@gmail.com>
2018-10-15 20:52:49 +0800

    Add ! pipe

--- npshell.c
+++ npshell.c
@@ -7,26 +7,13 @@
 #define MAX_COMMAND_LENGTH 256
 #define MAX_INPUT_LENGTH 256
 #define MAX_PIPE_NUM 256
+#define UNKNOWN_COMMAND_ERROR -1
 
-void print_env(int argc, const char *argv[])
-{
-  if (argc < 2) {
-    fputs("Arg num error!!\n", stderr);
-    return;
-  }
-  printf("%s\n", getenv(argv[1]));
-}
+struct NUM_PIPE {
+  int fd, count;
+};
 
-void set_env(int argc, const char *argv[])
-{
-  if (argc < 3) {
-    fputs("Arg num error!!\n", stderr);
-    return;
-  }
-  setenv(argv[1], argv[2], 1);
-}
-
-int call(const char *argv[], const char *envp[], int input)
+int call(const char *argv[], const char *envp[], int input, int error)
 {
   int pfd[2], pid = 0, status;
 
@@ -36,7 +23,11 @@ int call(const char *argv[], const char *envp[], int input)
     close(pfd[0]);
     dup2(input, 0);
     dup2(pfd[1], 1);
-    execvpe(argv[0], argv, envp);
+    if (error) dup2(pfd[1], 2);
+    if (execvpe(argv[0], argv, envp) == -1) {
+      fprintf(stderr, "Unknown command: [%s].\n", argv[0]);
+      exit(UNKNOWN_COMMAND_ERROR);
+    }
     exit(0);
   }
   else {
@@ -44,13 +35,10 @@ int call(const char *argv[], const char *envp[], int input)
     wait(&status);
     close(input);
     dup2(pfd[0], input);
+    close(pfd[0]);
   }
   return status;
 }
-
-struct NUM_PIPE {
-  int fd, count;
-};
 
 void push(int input, struct NUM_PIPE *np, int top, int num)
 {
@@ -73,12 +61,13 @@ int pop(int input, struct NUM_PIPE *np, int top)
       if (pipe(pfd) < 0) return -1;
       close(input);
       dup2(pfd[0], input);
+      close(pfd[0]);
       flag = 0;
     }
     while (len = read(np[top-1].fd, buf, MAX_INPUT_LENGTH)) {
       write(pfd[1], buf, len);
     }
-    top--;
+    close(np[--top].fd);
   }
   if (flag == 0) close(pfd[1]);
   return top;
@@ -91,7 +80,7 @@ int main(int argc, const char *argv[], const char *envp[])
   setbuf(stdout, NULL);
   setenv("PATH", "bin:.", 1);
 
-  char buf[MAX_INPUT_LENGTH];
+  char buf[MAX_INPUT_LENGTH], bak[MAX_INPUT_LENGTH];
   char **_argv;
   int _argc;
   wordexp_t exp;
@@ -99,14 +88,15 @@ int main(int argc, const char *argv[], const char *envp[])
   int num, top = 0;
 
   while (fputs("% ", stdout), fgets(buf, MAX_INPUT_LENGTH, stdin)) {
-    buf[strcspn(buf, "\n")] = 0;
+    buf[strcspn(buf, "\n\r")] = 0;
     if (buf[0] == 0) continue;
+    strcpy(bak, buf);
 
     char *p = buf, *command, *filename = NULL, *ptr;
     int input = dup(0), output = dup(1), len, status = -1, num = 0;
 
     top = pop(input, np, top);
-    while (command = strsep(&p, "|")) {
+    while (command = strsep(&p, "|!")) {
       if (ptr = strchr(command, '>')) *ptr = 0, filename = ptr + 2;
       if (isdigit(command[0]) && sscanf(command, "%d", &num)) {
         push(input, np, top++, num);
@@ -117,14 +107,14 @@ int main(int argc, const char *argv[], const char *envp[])
       _argc = exp.we_wordc;
       _argv = exp.we_wordv;
 
-      if (strcmp(_argv[0], "printenv") == 0) print_env(_argc, _argv);
-      else if (strcmp(_argv[0], "setenv") == 0) set_env(_argc, _argv);
+      if (strcmp(_argv[0], "printenv") == 0) printf("%s\n", getenv(_argv[1]));
+      else if (strcmp(_argv[0], "setenv") == 0) setenv(_argv[1], _argv[2], 1);
       else if (strcmp(_argv[0], "exit") == 0) return 0;
-      else status = call(_argv, envp, input);
+      else status = call(_argv, envp, input, p && bak[p-buf-1] == '!');
     }
 
     if (filename) close(output), output = open(filename, O_WRONLY|O_CREAT, 0644);
-    while(status == 0 && num == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
+    while (status == 0 && num == 0 && (len = read(input, buf, MAX_INPUT_LENGTH))) {
       write(output, buf, len);
     }
 
