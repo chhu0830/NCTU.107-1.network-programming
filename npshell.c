fab5eb63e94ded212af0264455c819eeee8080d0
Chia-Hsuan Hu <chhu0830@gmail.com>
2018-10-17 00:17:24 +0800

    Add pipe

--- npshell.c
+++ npshell.c
@@ -49,12 +49,12 @@ void parse_args(struct PROCESS *process)
   if (ptr = strchr(cmd, '>')) {
     *ptr = '\0';
     strcpy(process->filename, ptr+2);
-    process->output = open(filename, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    process->output = open(process->filename, O_WRONLY|O_TRUNC|O_CREAT, 0644);
   } else if (ptr = strchr(cmd, '!')) {
     *ptr = '\0';
     sscanf(ptr+1, "%d", &process->num);
     process->error = 1;
-  } else if (isdigit(cmd[0])) {
+  } else if (process->count != 1 && isdigit(cmd[0])) {
     sscanf(cmd, "%d", &process->num);
     process->count -= 1;
   }
@@ -73,6 +73,39 @@ void parse_args(struct PROCESS *process)
   }
 }
 
+void exec_cmds(struct PROCESS *process)
+{
+  int pid;
+  int fd[2];
+  char buf[8];
+  while (pipe(fd) < 0);
+  for (int i = 0; i < process->count; i++) {
+    while (pipe(process->cmds[i].fd) < 0);
+    while ((pid = fork()) < 0);
+    if (pid == 0) {
+      if (i == 0) dup2(process->input, 0);
+      else dup2(process->cmds[i-1].fd[0], 0);
+      if (i == process->count - 1) dup2(process->output, 1);
+      else dup2(process->cmds[i].fd[1], 1);
+
+      write(fd[1], "0", 1);
+      if (execvp(process->cmds[i].argv[0], process->cmds[i].argv) == -1) {
+        fprintf(stderr, "Unknown command: [%s].\n", process->cmds[i].argv[0]);
+        exit(UNKNOWN_COMMAND_ERROR);
+      }
+      exit(0);
+    } else {
+      while (read(fd[0], buf, 1) == 0);
+      if (i == 0) close(process->input);
+      else close(process->cmds[i-1].fd[0]);
+      if (i == process->count - 1) close(process->output), close(process->cmds[i].fd[0]);
+      close(process->cmds[i].fd[1]);
+    }
+  } 
+  close(fd[0]);
+  close(fd[1]);
+}
+
 int main(int argc, const char *argv[])
 {
   setbuf(stdout, NULL);
@@ -88,9 +121,12 @@ int main(int argc, const char *argv[])
     char filename[MAX_FILENAME_LENGTH];
     struct PROCESS process;
     memset(&process, 0, sizeof(process));
+    process.input = dup(0);
+    process.output = dup(1);
 
     parse_pipe(&process, buf);
     parse_args(&process);
+    exec_cmds(&process);
 
   }
 
